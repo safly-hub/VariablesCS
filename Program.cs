989a08995c4bdc237ef61a02cec69c9d4f747445
Lucas Frank Safly <lucafranksafly@lucasfranksafly.localdomain>
2020-08-11 16:44:03 -0400

    git push

--- Program.cs
+++ Program.cs
@@ -7,6 +7,29 @@ namespace VariablesCS
     static void Main(string[] args)
     {
       Console.WriteLine("Welcome to C#");
+      var numberOfCupsOfCoffee = '1';
+      var fullName = "Luke Safly";
+      var today = "August, 11th";
+      Console.WriteLine(numberOfCupsOfCoffee + " " + fullName + " " + today);
+      Console.Write("What is your name? ");
+      var userName = Console.ReadLine();
+      Console.WriteLine("It is a pleasure to meet you," + " " + userName);
+      Console.Write("How old are you? ");
+      var firstNumberAsString = Console.ReadLine();
+      var firstOperand = double.Parse(firstNumberAsString);
+      Console.Write("What is your shoe size? ");
+      var secondNumberAsString = Console.ReadLine();
+      var secondOperand = double.Parse(secondNumberAsString);
+      var sum = secondOperand + firstOperand;
+      Console.WriteLine(sum);
+      var product = secondOperand * firstOperand;
+      Console.WriteLine(product);
+      var difference = secondOperand - firstOperand;
+      Console.WriteLine(difference);
+      var quotient = secondOperand / firstOperand;
+      Console.WriteLine(quotient);
+      var remainder = secondOperand % firstOperand;
+      Console.WriteLine(remainder);
     }
   }
 }
