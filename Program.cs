73f1568852c0188e0fb06894eb42b348793a227c
LUCAS SAFLY <saflycorp@gmail.com>
2021-02-04 13:01:54 -0500

    I think its done?

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome to C#");
+                var numberOfCupsOfCoffee = '1';
+                const string V = "Lucas Frank Safly";
+                var fullName = V;
+                var today = "February, 4th";
+                Console.WriteLine(numberOfCupsOfCoffee + " " + fullName + " " + today);
+                Console.Write("What is your name? ");
+                var userName = Console.ReadLine();
+                Console.WriteLine("It is a pleasure to meet you," + " " + userName);
+                Console.Write("How old are you? ");
+                var firstNumberAsString = Console.ReadLine();
+                var firstOperand = double.Parse(firstNumberAsString);
+                Console.Write("What is your shoe size? ");
+                var secondNumberAsString = Console.ReadLine();
+                var secondOperand = double.Parse(secondNumberAsString);
+                var sum = secondOperand + firstOperand;
+                Console.WriteLine(sum);
+                var product = secondOperand * firstOperand;
+                Console.WriteLine(product);
+                var difference = secondOperand - firstOperand;
+                Console.WriteLine(difference);
+                var quotient = secondOperand / firstOperand;
+                Console.WriteLine(quotient);
+                var remainder = secondOperand % firstOperand;
+                Console.WriteLine(remainder);
+            }
+        }
     }
-  }
 }
